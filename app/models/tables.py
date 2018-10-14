5f55d550dd49980fd5e3aae66d5d3a7c7e695e3d
PavaniVitor <vitorpavani@poli.ufrj.br>
2018-10-14 16:07:14 -0300

    adiciona tables.py aos models

---
+++ app/models/tables.py
@@ -0,0 +1,36 @@
+from app import db
+
+
+class User(db.model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer , primary_key=True)
+    username = db.Column(db.String , unique = True)
+    password = db.Column(db.String)
+    car_plate = db.Column(db.String)
+    email = db.Column(db.String , unique = True)
+
+    def __init__ (self, username, password , car_plate , email)
+        self.username = username
+        self.password = password
+        self.car_plate = car_plate
+        self.email = email
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer , primary_key = True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer , db.ForeignKey('users.id'))
+    owner = db.relationship('User' , foreign_keys = user_id)
+
+    def __init__(self, content , user_id)
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
