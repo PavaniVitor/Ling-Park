e223c49ca2fcde3ba55138bf1b7ad454a3bfaad4
PavaniVitor <vitorpavani@poli.ufrj.br>
2018-10-16 14:33:32 -0300

    adiciona um exemplo de busca de usuario no banco

--- app/controllers/teste.py
+++ app/controllers/teste.py
@@ -3,11 +3,14 @@ from app import app
 from app import db
 from app.models.tables import User, Post
 
+
 #exemplo de arquivo de controller!
 
 @app.route('/teste')
 def echo_teste():
+    print(logged_user)
     return 'ola denovo!'
+
 
 @app.route('/users')
 def echo_view_users():
@@ -15,3 +18,29 @@ def echo_view_users():
     users = User.query.order_by(User.username).all()
     print(users[0].username)
     return render_template('users.html' , users = users)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/echo_login' , methods = ['POST' , 'GET'])
+def login_echo():
+    user_name = request.form['name']
+    user_password = request.form['password']
+
+    logged_user = User.query.filter_by(username = user_name).first()
+    print(logged_user)
+    if (logged_user != None):#checa se o usuario existe no banco de dados
+        return 'eaeae   ' + str(logged_user.username) 
+    else:
+        return 'errrrrrrowwwwwwwwww' # vai pra pagina de erro no login
+
+
+@app.route('/posts' , methods = ['POST' , 'GET'])
+def posts():
+    if request.method == 'POST':
+        print('post')
+        #posta a mensagem
+    return 
+    
+    #mostra as mensagens do banco 
