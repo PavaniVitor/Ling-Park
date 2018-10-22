c175ee943e5631e009fd98b07eea58e491b8d3b4
Yuri Reis Schaider <yurischaider@users.noreply.github.com>
2018-10-22 16:48:07 -0300

    criação do home

---
+++ app/controllers/home.py
@@ -0,0 +1,12 @@
+from flask import render_template, url_for, flash
+from app import app, db
+from app.models.tables import Post
+
+
+@app.route('/home')
+def home():
+    lista_posts = Post.query.order_by(Post.id).all()
+    lista_posts.reverse()
+    lista_posts = lista_posts[:20 - len(lista_posts)]
+    return render_template('home.html' , posts=lista_posts)
+    
