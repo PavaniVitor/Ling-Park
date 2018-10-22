e03e08dd9b157c3f9f214885e70db69425f9ea96
Yuri Reis Schaider <yurischaider@users.noreply.github.com>
2018-10-22 14:36:33 -0300

    Adiciona o formulario de registro

--- app/models/forms.py
+++ app/models/forms.py
@@ -2,10 +2,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import PasswordField
 from wtforms import BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired , ValidationError, EqualTo, Email
+from app.models.tables import User
 
 
 class LoginForm(FlaskForm):
     username = StringField('username' , validators=[DataRequired()])
     password = PasswordField('password' , validators=[DataRequired()])
     remember_me = BooleanField('remember_me')
+
+class RegForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    car_plate = StringField('Placa do carro') #adicionar  metodovalidador para a placa validate_car_plate
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
