1e4600c57141ad817979f071c34741d32d62d2d5
Yuri Reis Schaider <yurischaider@users.noreply.github.com>
2018-10-22 14:37:19 -0300

    conserta um nome errado de variavel

--- app/models/tables.py
+++ app/models/tables.py
@@ -27,7 +27,7 @@ class User(db.Model):
         return str(self.id)
     
     def set_password(self, password):
-        self.password = generate_password_hash(_password)
+        self.password = generate_password_hash(password)
 
     def check_password(self , password):
         return check_password_hash(self.password, password)
