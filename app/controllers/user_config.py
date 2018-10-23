c97d60642f46f5380e44d2715e5810cab608ad23
PavaniVitor <vitorpavani@poli.ufrj.br>
2018-10-22 22:58:36 -0300

    adiciona as informações do usuario na pagina preferences

--- app/controllers/user_config.py
+++ app/controllers/user_config.py
@@ -1,10 +1,12 @@
-from flask import render_template, request
+from flask import render_template, request , redirect , url_for
 from app import app
 from app import db
 from app.models.tables import User, Post
-from flask_login import login_required
+from flask_login import login_required , current_user , login_manager
 
-@login_required
 @app.route('/config')
+@login_required
 def config():
-    return render_template('preferences.html')
+
+    user = current_user
+    return render_template('preferences.html' , user = user)
